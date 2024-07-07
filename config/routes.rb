11c38cca7bd3664980ffed452549a91efb5128e7
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-07-08 04:44:35 +0800

    feat(healthcheck): expose /up endpoint and set as root for Rails health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+  root to: "health#show"
   devise_for :users, controllers: {
     sessions: 'sessions',
     registrations: 'registrations',
