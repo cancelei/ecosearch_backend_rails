90c944a7e80ade52a72a73fcadc7bc12c6087cce
cancelei <glauber.bannwart@gmail.com>
2024-07-06 22:36:00 -0300

    chore: Add CSRF endpoint for API v1

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      get 'csrf', to: 'csrf#index'
       resource controller: 'search' do
         collection do
           post :search
