3f555b3e0f0993757d2dbd3d05b1d8ea418ec0dc
cancelei <glauber.bannwart@gmail.com>
2024-07-06 22:21:52 -0300

    chore: Update CSRF protection in base controller and add CSRF endpoint

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class BaseController < ApplicationController
-      # before_action :authenticate_user!
+      before_action :authenticate_user!
       protect_from_forgery with: :exception
       skip_before_action :verify_authenticity_token, if: :json_request?
 
