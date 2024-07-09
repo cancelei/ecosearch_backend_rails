ae661895b529ab3679dda8dbe92744c5acbcd733
cancelei <glauber.bannwart@gmail.com>
2024-07-09 14:21:01 -0300

    add url for react front-end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000' # Replace with your React app URL
+    origins 'http://localhost:3000', 'http://localhost:5173'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
